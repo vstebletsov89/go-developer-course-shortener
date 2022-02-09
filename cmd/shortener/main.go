14019c8588cd6d6aa7263308407df6fe8d4a8c78
vstebletsov89 <vstebletsov89@github.com>
2022-02-09 17:39:00 +0100

    switch to chi router

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"go-developer-course-shortener/configs"
 	"go-developer-course-shortener/internal/app/handlers"
 	"log"
@@ -11,8 +13,18 @@ import (
 func main() {
 	log.SetOutput(os.Stdout)
 	log.Printf("Server started on %v\n", configs.ServerAddress)
+	r := chi.NewRouter()
+
+	// зададим встроенные middleware, чтобы улучшить стабильность приложения
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
 	// маршрутизация запросов обработчику
-	http.HandleFunc("/", handlers.HandlerShortener)
+	r.Get("/", handlers.HandlerShortener)
+	r.Post("/", handlers.HandlerShortener)
+
 	// запуск сервера с адресом localhost, порт 8080
-	log.Fatal(http.ListenAndServe(configs.ServerAddress, nil))
+	log.Fatal(http.ListenAndServe(configs.ServerAddress, r))
 }
