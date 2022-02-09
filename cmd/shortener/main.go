ee32c882c8d3b5f8a682036145f4f52b2a053cfc
vstebletsov89 <vstebletsov89@github.com>
2022-02-09 20:19:11 +0100

    remove middlewares

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 	"go-developer-course-shortener/configs"
 	"go-developer-course-shortener/internal/app/handlers"
 	"log"
@@ -14,12 +13,6 @@ func main() {
 	log.SetOutput(os.Stdout)
 	log.Printf("Server started on %v\n", configs.ServerAddress)
 	r := chi.NewRouter()
-
-	// зададим встроенные middleware, чтобы улучшить стабильность приложения
-	r.Use(middleware.RequestID)
-	r.Use(middleware.RealIP)
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
 
 	// маршрутизация запросов обработчику
 	r.Post("/", handlers.HandlerPOST)
