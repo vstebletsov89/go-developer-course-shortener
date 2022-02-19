ed6eae3d575c682496ea1dbd1ab83d4e477a90bd
vstebletsov89 <vstebletsov89@github.com>
2022-02-19 21:13:39 +0100

    fix tag

--- internal/configs/env.go
+++ internal/configs/env.go
@@ -8,7 +8,7 @@ import (
 type Config struct {
 	ServerAddress   string `env:"SERVER_ADDRESS" envDefault:"localhost:8080"`
 	BaseURL         string `env:"BASE_URL" envDefault:"http://localhost:8080"`
-	FileStoragePath string `env:"FILE_STORAGE_PATH" envDefault:"FILE_STORAGE_PATH_NOT_DEFINED""`
+	FileStoragePath string `env:"FILE_STORAGE_PATH" envDefault:"FILE_STORAGE_PATH_NOT_DEFINED"`
 }
 
 func ReadConfig() *Config {
